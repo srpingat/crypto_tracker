8d191e3e96b955d09bb321010e932369e4726388
SHRUTI RAHUL PINGAT <109078275+srpingat@users.noreply.github.com>
2024-07-27 06:57:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,37 +21,7 @@ function App() {
   var cursor;
   var cursorPointer;
 
-  // useEffect(() => {
-  //   cursor = document.getElementById("cursor");
-  //   cursorPointer = document.getElementById("cursor-pointer");
-
-  //   document.body.addEventListener("mousemove", function (e) {
-  //     return (
-  //       (cursor.style.left = e.clientX + "px"),
-  //       (cursor.style.top = e.clientY + "px"),
-  //       (cursorPointer.style.left = e.clientX + "px"),
-  //       (cursorPointer.style.top = e.clientY + "px")
-  //     );
-  //   });
-
-  //   document.body.addEventListener("mousedown", function (e) {
-  //     return (
-  //       (cursor.style.height = "0.5rem"),
-  //       (cursor.style.width = "0.5rem"),
-  //       (cursorPointer.style.height = "3rem"),
-  //       (cursorPointer.style.width = "3rem")
-  //     );
-  //   });
-
-  //   document.body.addEventListener("mouseup", function (e) {
-  //     return (
-  //       (cursor.style.height = "0.3rem"),
-  //       (cursor.style.width = "0.3rem"),
-  //       (cursorPointer.style.height = "2rem"),
-  //       (cursorPointer.style.width = "2rem")
-  //     );
-  //   });
-  // }, []);
+ 
 
   return (
     <div className="App">
